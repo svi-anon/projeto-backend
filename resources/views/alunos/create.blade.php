e4b98312ce48f409726f6ad55f1e41a054e42b45
svi-anon <ep.ruaro@protonmail.com>
2026-09-09 23:11:57 -0300

    Pt 14 - Formulario aluno

--- resources/views/alunos/create.blade.php
+++ resources/views/alunos/create.blade.php
@@ -1,7 +1,22 @@
 @extends('layouts.app')
 
-@section('title', 'Criar aluno')
+@section('title', 'Cadastrar aluno')
 
 @section('content')
-<h1>Criar aluno</h1>
+<h1>Cadastrar aluno</h1>
+
+<form action="{{ route('alunos.store') }}" method="POST">
+    @csrf
+
+    <label>Nome</label>
+    <input type="text" name="nome">
+
+    <label>Email</label>
+    <input type="email" name="email">
+
+    <label>Curso</label>
+    <input type="text" name="curso">
+
+    <button type="submit">Cadastrar</button>
+</form>
 @endsection
