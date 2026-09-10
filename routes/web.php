f73f18c7f6940d805da2b66474edc4dc6926381c
svi-anon <ep.ruaro@protonmail.com>
2026-09-09 23:26:14 -0300

    Pt 21 - Middleware d role

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,47 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AlunoController;
+use App\Models\Turma;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'home')->name('home');
+
+Route::get('/sobre', function () {
+    return 'Pagina sobre';
 });
+
+Route::get('/contato', function () {
+    return 'Pagina de contato';
+});
+
+Route::get('/produto/{id}', function ($id) {
+    return "Produto {$id}";
+});
+
+Route::get('/categoria/{id}', function ($id) {
+    return "Categoria {$id}";
+});
+
+Route::get('/usuario/{id}', function ($id) {
+    return "Usuario {$id}";
+});
+
+Route::get('/turmas/{turma}', function (Turma $turma) {
+    return view('turmas.show', compact('turma'));
+})->name('turmas.show');
+
+Route::get('/admin', function () {
+    return 'Area admin';
+})->middleware(['auth', 'role:admin']);
+
+Route::get('/professor', function () {
+    return 'Area prof';
+})->middleware(['auth', 'role:prof']);
+
+Route::resource('alunos', AlunoController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__.'/auth.php';
