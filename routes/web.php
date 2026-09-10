fb12e4e5df8a053f506d1761027037a12ab57bcd
svi-anon <ep.ruaro@protonmail.com>
2026-09-09 22:48:03 -0300

    Pt 2 - Rotas c parametro

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,15 @@ Route::get('/alunos', function () {
 Route::get('/contato', function () {
     return 'Pagina de contato';
 });
+
+Route::get('/produto/{id}', function ($id) {
+    return "Produto {$id}";
+});
+
+Route::get('/categoria/{id}', function ($id) {
+    return "Categoria {$id}";
+});
+
+Route::get('/usuario/{id}', function ($id) {
+    return "Usuario {$id}";
+});
